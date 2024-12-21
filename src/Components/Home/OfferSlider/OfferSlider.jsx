1b9e03f0e8bf3935a6f3fa25699d093d09772d89
Anamul029 <anamhaque.5101@gmail.com>
2024-12-21 12:37:18 +0600

    adding homepage element

--- src/Components/Home/OfferSlider/OfferSlider.jsx
+++ src/Components/Home/OfferSlider/OfferSlider.jsx
@@ -37,7 +37,7 @@ const OfferSlider = () => {
         ]
     )
     return (
-        <div className="mt-2">
+        <div className="mt-2 px-[8%] mx-auto">
             <div ref={sliderRef} className="keen-slider">
                 <div className="keen-slider__slide number-slide1"><img src="https://www.startech.com.bd/image/cache/catalog/home/geyser-home-banner-982x500.webp" className="" alt="" /></div>
                 <div className="keen-slider__slide number-slide1"><img src="https://www.startech.com.bd/image/cache/catalog/home/gadget-fest-home-banner-2024-982x500.webp" alt="" /></div>
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,11 +1,15 @@
+import Footer from '../Footer/Footer';
+import DownNavBar from '../NavBar/DownNavBar';
 import NavBar from '../NavBar/NavBar';
 import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div className='w-[100%] px-3'>
+        <div className='max-w-[1900px] mx-auto bg-blue-50 px-3'>
             <NavBar></NavBar>
+            <DownNavBar></DownNavBar>
             <Outlet></Outlet>
+            <Footer/>
         </div>
     );
 };
